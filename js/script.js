6c3fb4bec4c42b4e7e8f0f32b73d7adc1712bd79
yurisukhomud <57061265+yurisukhomud@users.noreply.github.com>
2020-08-15 12:12:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+	$('.slider-small').slick({
+		arrows: true,
+		dots: false,
+		adaptiveHeight: false,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		speed: 500,
+		easing: 'linear',
+		infinite: true,
+		initialSlide: 0,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: false,
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		waitForAnimate: false,
+		centerMode: true,
+		variableWidth: true,
+		rows: 1,
+		asNavFor: ".slider-big",
+		focusOnSelect: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					speed: 1000,
+					autoplay: false,
+				}
+			}
+		]
+	});
+	$('.slider-big').slick({
+		arrows: false,
+		fade: true,
+		asNavFor: ".slider-small",
+		autoplaySpeed: 3000,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					fade: false,
+					speed: 1000,
+					autoplay: false,
+				}
+			}
+		]
+	});
+});
